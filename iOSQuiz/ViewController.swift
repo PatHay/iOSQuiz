8150be96d8a48806aec41fa9ffcf662f2fb5b8ac
Patrick Hayes <phayes2828@gmail.com>
2017-10-31 17:38:50 -0700

    Initial Commit

---
+++ iOSQuiz/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOSQuiz
+//
+//  Created by Patrick Hayes on 10/31/17.
+//  Copyright © 2017 Patrick Hayes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
